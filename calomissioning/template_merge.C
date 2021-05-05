bbcaf5b3880df747ed8bf55ffe8f8892f584a4e0
William Quinn <wquinn@cca008.in2p3.fr>
2021-05-05 16:12:01 +0200

    bug fixes

--- calomissioning/template_merge.C
+++ calomissioning/template_merge.C
@@ -5,19 +5,19 @@
 
     for ( int i = 0; i < 712; i++)
     {
-        string name_0=Form("Template_Ch%d.root",i);
+        string name_0=Form("Template_Ch%d",i);
         TH1D* hist_0 = (TH1D*)file_0->Get(name_0.c_str());
 
         if (hist_0->GetMean() > 0.0)
         {
-            out_file-cd();
-            hist_0.Write();
+            out_file->cd();
+            hist_0->Write();
         }else
         {
-            string name_1=Form("Template_Ch%d.root",i);
+            string name_1=Form("Template_Ch%d",i);
             TH1D* hist_1 = (TH1D*)file_1->Get(name_1.c_str());
-            out_file-cd();
-            hist_1.Write();
+            out_file->cd();
+            hist_1->Write();
 
             delete hist_1;
         }
