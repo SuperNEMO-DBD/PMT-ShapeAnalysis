696806749d2b0f351f167b53dbbf34fc2834e197
William Quinn <william.quinn.14@ucl.ac.uk>
2021-05-17 12:44:09 +0100

    first commit

--- pmt_he_study/xml_reader.py
+++ pmt_he_study/xml_reader.py
@@ -21,11 +21,17 @@ def main():
     pmt_array.set_pmt_templates('templates.root', ['Template_Ch0'])
     pmt_array.get_pmt_object_number(0).set_template_bool(True)
 
+    count = 0
+
     for i, waveform in enumerate(waveforms):
         pmt_waveform = PMT_Waveform(waveform, pmt_array.get_pmt_object_number(0))
 
         if pmt_waveform.get_pmt_pulse_times() > 0 and pmt_waveform.get_pmt_pulse_charge() < 100:
             plt.plot(pmt_waveform.get_pmt_waveform())
+            count += 1
+
+        if count == 10:
+            break
 
 
 if __name__ == '__main__':
