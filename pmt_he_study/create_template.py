d32641af92755b6615faa0992b39ab99b3a92ff5
William Quinn <william.quinn.14@ucl.ac.uk>
2021-05-19 23:26:50 +0100

    first commit

--- pmt_he_study/create_template.py
+++ pmt_he_study/create_template.py
@@ -54,8 +54,10 @@ def main():
                     apulse_event.set('ID', str(event_index))
                     apulse_event.set('apulse_num', str(len(pmt_waveform.get_pmt_pulse_times())))
                     apulse_waveform = ET.SubElement(apulse_event, 'waveform')
+                    apulse_waveform.set('CH', str(channel))
                     apulse_waveform.text(trace.firstChild.data)
                     apulse_times = ET.SubElement(apulse_event, 'apulse_times')
+                    apulse_times.set('CH', str(channel))
                     apulse_times.text(" ".join([str(i) for i in pmt_waveform.get_pmt_pulse_times()]))
 
                     counter += 1
