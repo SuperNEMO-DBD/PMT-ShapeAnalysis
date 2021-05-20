cbe6e206edfa40b87632d5280c51e9075c37d554
William Quinn <william.quinn.14@ucl.ac.uk>
2021-05-20 01:31:49 +0100

    first commit

--- pmt_he_study/create_template.py
+++ pmt_he_study/create_template.py
@@ -8,26 +8,7 @@ from xml.dom import minidom
 import xml.etree.ElementTree as ET
 
 
-def create_xml_file():
-    pass
-
-
-def _pretty_print(current, parent=None, index=-1, depth=0):
-    for i, node in enumerate(current):
-        _pretty_print(node, current, i, depth + 1)
-    if parent is not None:
-        if index == 0:
-            parent.text = '\n' + ('\t' * depth)
-        else:
-            parent[index - 1].tail = '\n' + ('\t' * depth)
-        if index == len(parent) - 1:
-            current.tail = '\n' + ('\t' * (depth - 1))
-
-
-def main():
-    args = io_parse_arguments()
-    input_file = args.i
-    output_file = args.o
+def create_xml_file(input_file: str):
 
     pmt_array = PMT_Array([1, 1], "test")
     # Set the cuts from a config file. This also sets up the histograms in the array format
@@ -69,6 +50,14 @@ def main():
                     apulse_times = ET.SubElement(apulse_event, 'apulse_times')
                     apulse_times.set('CH', str(channel))
                     apulse_times.text = " ".join([str(i) for i in pmt_waveform.get_pmt_pulse_times()])
+                    apulse_shapes = ET.SubElement(apulse_event, 'apulse_shapes')
+                    apulse_shapes.set('CH', str(channel))
+                    apulse_shapes.text = " ".join([str(i) for i in pmt_waveform.pmt_waveform_sweep_shape[
+                        pmt_waveform.get_pmt_pulse_times()-pmt_waveform.get_pmt_object().get_sweep_range()[0]]])
+                    apulse_amps = ET.SubElement(apulse_event, 'apulse_amps')
+                    apulse_amps.set('CH', str(channel))
+                    apulse_amps.text = " ".join([str(i) for i in pmt_waveform.pmt_waveform_sweep_amp[
+                        pmt_waveform.get_pmt_pulse_times() - pmt_waveform.get_pmt_object().get_sweep_range()[0]]])
 
                     counter += 1
                 else:
@@ -85,5 +74,42 @@ def main():
     tree.write("apulses.xml")
 
 
+def _pretty_print(current, parent=None, index=-1, depth=0):
+    for i, node in enumerate(current):
+        _pretty_print(node, current, i, depth + 1)
+    if parent is not None:
+        if index == 0:
+            parent.text = '\n' + ('\t' * depth)
+        else:
+            parent[index - 1].tail = '\n' + ('\t' * depth)
+        if index == len(parent) - 1:
+            current.tail = '\n' + ('\t' * (depth - 1))
+
+
+def main():
+    args = io_parse_arguments()
+    input_file = args.i
+    output_file = args.o
+
+    print(">>> Parsing the data file...")
+    processing_start = time.time()
+    # parse an xml file by name
+    tree = ET.ElementTree()
+    tree.parse(input_file)
+    root = tree.getroot()
+    events = root.findall('event')
+    parse_time = time.time() - processing_start
+    print(">>> File is good. Parse time: %.3f s" % parse_time)
+    print(">>> Number of Events: {}".format(len(events)))
+
+    for event_index, event in tqdm.tqdm(enumerate(events)):
+        waveform = event.find('waveform')
+        apulse_times = event.find('apulse_times')
+        apulse_num = event.attrib['apulse_num']
+
+        print(apulse_num)
+
+
 if __name__ == '__main__':
-    main()
+    # main()
+    create_xml_file('/unix/nemo4/PMT_He_Study_nemo4/data/raw_xml_files/200404/A1400_B1400_t1128.xml')
