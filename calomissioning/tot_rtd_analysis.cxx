c556a58b96815dd6958bfe1704f287beca0aab4f
William Quinn <williamquinn@eduroam-int-dhcp-97-163-134.ucl.ac.uk>
2021-10-04 17:56:55 +0100

    update

--- calomissioning/tot_rtd_analysis.cxx
+++ calomissioning/tot_rtd_analysis.cxx
@@ -168,16 +168,16 @@ int main(int argc, char **argv)
         std::cout<<"Settings:"          <<std::endl;
         std::cout<<"tdc2ns: "           <<tdc2ns<<std::endl;
         std::cout<<"adc2mv: "           <<adc2mv<<std::endl;
-        std::cout<<"sweep_start: "      <<config_object.sweep_start<<std::endl;
-        std::cout<<"pre_trigger: "      <<config_object.pre_trigger<<std::endl;
-        std::cout<<"trigger: "          <<config_object.trigger<<std::endl;
-        std::cout<<"trig_tolerance: "   <<config_object.trig_tolerance<<std::endl;
-        std::cout<<"apulse_time_cut: "  <<config_object.apulse_time_cut<<std::endl;
-        std::cout<<"shape_cut: "        <<config_object.shape_cut<<std::endl;
-        std::cout<<"amp_cut: "          <<config_object.amp_cut<<std::endl;
-        std::cout<<"charge_cut: "       <<config_object.charge_cut<<std::endl;
+        // std::cout<<"sweep_start: "      <<config_object.sweep_start<<std::endl;
+        // std::cout<<"pre_trigger: "      <<config_object.pre_trigger<<std::endl;
+        // std::cout<<"trigger: "          <<config_object.trigger<<std::endl;
+        // std::cout<<"trig_tolerance: "   <<config_object.trig_tolerance<<std::endl;
+        // std::cout<<"apulse_time_cut: "  <<config_object.apulse_time_cut<<std::endl;
+        // std::cout<<"shape_cut: "        <<config_object.shape_cut<<std::endl;
+        // std::cout<<"amp_cut: "          <<config_object.amp_cut<<std::endl;
+        // std::cout<<"charge_cut: "       <<config_object.charge_cut<<std::endl;
         std::cout<<"resistance: "       <<config_object.resistance<<std::endl;
-        std::cout<<"integration: "      <<config_object.integration[0]<<"-"<<config_object.integration[1]<<std::endl;
+        // std::cout<<"integration: "      <<config_object.integration[0]<<"-"<<config_object.integration[1]<<std::endl;
         std::cout<<"template_file: "    <<config_object.template_file<<std::endl;
 
         sncabling::service snCabling;
