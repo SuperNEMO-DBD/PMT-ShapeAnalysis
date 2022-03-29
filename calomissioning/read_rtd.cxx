20f252c1c91fc19ff9c39ea3c72bc36161fcb2d8
William Quinn <williamquinn@eduroam-int-dhcp-97-163-134.ucl.ac.uk>
2022-03-29 12:13:44 +0100

    trying to get tdc to work

--- calomissioning/read_rtd.cxx
+++ calomissioning/read_rtd.cxx
@@ -332,7 +332,8 @@ int main(int argc, char **argv)
 	            int32_t  chip_num        = calo_hit.get_chip_num();   // Chip number (0-7)
 	            auto     hit_num         = calo_hit.get_hit_num();
 
-                eventn.tdc = tdc;
+                eventn.tdc = tdc * tdc2ns;
+                std::cout << eventn.tdc << std::endl
 
 	            // Extract SAMLONG channels' data:
 	            // 2 channels per SAMLONG
